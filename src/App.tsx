20b9a2d245103b9385181bc7ed7342001c0f804d
Antero Vaissi <avaissi@gmail.com>
2024-06-26 13:21:17 +0300

    Add error handling for add todo with message toast. Update dark mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import "./App.css"
+import { ToastContainer } from "react-toastify";
 import { TodoList } from "./components/todoList";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css"
 
 function App() {
 
+
   return (
     <div className="main">
-      <TodoList />
+      <TodoList/>
+      <ToastContainer/>
     </div>
   )
 }
--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,22 +1,36 @@
 import { useCallback, useMemo } from "react";
 
+import { toast } from "react-toastify";
 import { TodoItem } from "./todoItem";
 import { TodoModel } from "./types";
 import { useGetTodosQuery, useAddTodoMutation } from "../api/apiSlice.ts";
 
 
 export const TodoList = () => {
+  // @ts-expect-error: Unknown return types from RTK Slice
   const {data: todos = [], isSuccess} = useGetTodosQuery();
-  const [addTodo] = useAddTodoMutation();
+  const [addTodo, {reset: resetAddTodoError}] = useAddTodoMutation();
 
   const handleAddTodo = useCallback(() => {
-    addTodo({
-      content: "New Todo item",
-      created_at: new Date(Date.now()).toISOString(),
-      id: null,
-      done_at: null,
-    } satisfies TodoModel).unwrap();
-  }, [addTodo])
+    const handleAddTodo = async () => {
+      try {
+        await addTodo({
+          content: "New Todo item",
+          created_at: new Date(Date.now()).toISOString(),
+          id: null,
+          done_at: null,
+        } satisfies TodoModel).unwrap();
+      } catch (error) {
+        console.error("Error adding todo:" + JSON.stringify(error));
+        toast.error("Error while trying to add a Todo", {
+          position: "bottom-right",
+          onClose: resetAddTodoError,
+        });
+      }
+    }
+
+    handleAddTodo()
+  }, [addTodo, resetAddTodoError])
 
   const sortedTodos = useMemo(() => {
     const sortedTodos = isSuccess ? [...todos] : [];
@@ -29,10 +43,10 @@ export const TodoList = () => {
 
   return (
     <>
-      <h2 className={"header-center"}>TODO LIST</h2>
+      <h2 className={ "header-center" }>TODO LIST</h2>
       <button onClick={ handleAddTodo }>Add Todo</button>
-      {sortedTodos && sortedTodos.length === 0 && <h4 className={"no-items-header"}>No Todo items</h4>}
-      <div className={"todo-list"}>
+      { sortedTodos && sortedTodos.length === 0 && <h4 className={ "no-items-header" }>No Todo items</h4> }
+      <div className={ "todo-list" }>
         { sortedTodos &&
           sortedTodos.length > 0 &&
           sortedTodos.map((todo) => <TodoItem key={ todo.id } item={ todo }/>) }
